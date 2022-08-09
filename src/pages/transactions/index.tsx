bf3240dfe63dd6519c606ae1e9e4c468ade92aab
Renato Lomba <rntlomba@gmail.com>
2022-08-09 18:06:16 -0300

    Estrutura visual - Componente: Summary

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
 
 import { Header } from '../../components/header'
+import { Summary } from '../../components/summary'
 import { TransactionsContainer } from './styles'
 
 export const TransactionsPage: FC = () => {
   return (
     <TransactionsContainer>
       <Header />
+
+      <Summary />
 
       <h1>Transactions</h1>
     </TransactionsContainer>
