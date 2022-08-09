235bf212dc8ad80b814cd15b60e08ce2c306e82e
Renato Lomba <rntlomba@gmail.com>
2022-08-09 18:36:22 -0300

    Estrutura visual - Tabela de transações

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -2,16 +2,33 @@ import { FC } from 'react'
 
 import { Header } from '../../components/header'
 import { Summary } from '../../components/summary'
-import { TransactionsContainer } from './styles'
+import { TransactionsContainer, TransactionsTable } from './styles'
 
 export const TransactionsPage: FC = () => {
   return (
-    <TransactionsContainer>
+    <div>
       <Header />
 
       <Summary />
 
-      <h1>Transactions</h1>
-    </TransactionsContainer>
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td className="input">R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td className="output">R$ 1.200,00</td>
+              <td>Casa</td>
+              <td>15/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   )
 }
